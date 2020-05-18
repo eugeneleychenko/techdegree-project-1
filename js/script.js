ff100542072c4eb8ed93420bf68dd8435bfdd28f
eugeneleychenko <nycspicebo@gmail.com>
2020-05-18 12:02:19 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,39 +10,81 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/	
-const quotes = ["The stock market is filled with individuals who know the price of everything, but the value of nothing","You get recession, you get stock market declines. If you don’t understand that’s going to happen, then you are not ready and you will not do well in the markets.","An investment in knowledge pays the best interest.","I will tell you how to become rich. Close the doors. Be fearful when others are greedy. Be greedy when others are fearful.","A thousand point gain or a thousand point decline does not alter the fact that we are saving for retirement or building up funds for education", "People’s everyday routines will keep the stock market going.", "The stock market is the only market that when prices are slashed, clients run away!", "Know what you own, and know why you own it."]
-const source = ["Phillip Fisher","Peter Lynch","Benjamin Franklin","Warren Buffett","Don Connelly","Don Connelly","Don Connelly","Peter Lynch"]
-const citation = ["Wall Street Journal",,,,,"New York Times",,,]
-const year = [1988,,,1992,,,,,]
+const quotes = [ // Create an array of objects to hold the data for your quotes. Name the array ​quotes.​
+{
+	quote: "The stock market is filled with individuals who know the price of everything, but the value of nothing",
+	source: "Phillip Fisher",
+	cite: "Wall Street Journal",
+	yr: 1988,
+},
+{
+	quote: "You get recession, you get stock market declines. If you don’t understand that’s going to happen, then you are not ready and you will not do well in the markets.",
+	source: "Peter Lynch",
+	cite: "Huff Po",
+	yr: 2001,
+},
+{
+	quote: "An investment in knowledge pays the best interest.",
+	source: "Benjamin Franklin",
+	cite: "ABC",
+	yr:2004,
+},
+{
+	quote: "I will tell you how to become rich. Close the doors. Be fearful when others are greedy. Be greedy when others are fearful.",
+	source:"Warren Buffett",
+	cite:"NBC",
+	yr:1992,
+},
+{
+	quote: "A thousand point gain or a thousand point decline does not alter the fact that we are saving for retirement or building up funds for education", 
+	source: "Don B Connelly",
+	cite: "CBS",
+	yr: 1982,
+},
+{
+	quote: "People’s everyday routines will keep the stock market going.",
+	source: "Don C Connelly",
+	cite: "New York Times",
+	yr: 1984,
+}
+];
+
+
 
 /***
  * `getRandomQuote` function
 ***/
- function getRandomQuote()  {
- 	var quoteRandom = Math.floor( Math.random() * length.quotes ) +1;
- 	var finQuote = quotes[quoteRandom];
- 	return finQuote; 
- 	
-
+ var quoteNumber = 0;
+ function getRandomQuote()  { // Create a function named g​etRandomQuote​ and inside that function:
+ 	quoteNumber = Math.floor( Math.random() * quotes.length ) ; // generate a random number between 0 and the last index in the ​array​ parameter
+ 	var quoteRandom = quotes[quoteNumber].quote; //Uses it to select a quote object from the quotes array with box notation.
+ 	return quoteRandom; // return the random item
+ 	//console.log(quoteRandom);
  }
+ //console.log(quoteNumber);
 
 
 
-/***
- * `printQuote` function
-***/
+
+// /***
+//  * `printQuote` function
+// ***/
+var html = ""; //Create a variable to store the HTML string that contains the quote and source properties using the example HTML in the instructions.
 function printQuote()  {
-
-document.querySelector("p.quote").textContent = getRandomQuote();
+var newQuote = getRandomQuote(); //create a variable that calls the getRandomQuote()​ function
+html = '<p class="quote">' + quotes[quoteNumber].quote + '</p>';
+html += '<p class="source">' + quotes[quoteNumber].source + '<span class="citation">' + quotes[quoteNumber].cite + '</span><span class="year">' + quotes[quoteNumber].yr + '</span></p>';
+document.getElementById("quote-box").innerHTML = html; //Set innerHTML property of the '​ quote-box'​ div to equal the HTML string.
 }
 
 
 
 
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+
+// /***
+//  * click event listener for the print quote button
+//  * DO NOT CHANGE THE CODE BELOW!!
+// ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
